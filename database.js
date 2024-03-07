5199a46bf45b416f465785a53dd8d71d38eefac8
Jonas Achouri Sihlén <62463923+nimblegeek@users.noreply.github.com>
2024-03-07 09:51:43 +0000

    database updates

--- database.js
+++ database.js
@@ -1,11 +1,13 @@
-// database.js
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const dbPath = path.resolve("../../dojolist.db");
 
-async function openDb() {
-  return open({
-    filename: '/dojolist.db',
-    driver: sqlite3.Database
+function openDb() {
+  return new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the SQlite database.");
   });
 }
 
--- pages/api/data.js
+++ pages/api/data.js
@@ -1,9 +1,9 @@
 // pages/api/data.js
-const { openDb } = require('../../database');
+const { openDb } = require("../../database.js");
 
 export default async function handler(req, res) {
   const db = await openDb();
 
-  const data = await db.all('SELECT * FROM dojo');
+  const data = await db.all("SELECT * FROM dojo");
   res.status(200).json(data);
 }
